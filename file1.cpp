4c61fed59bd238fdedf39bd0adba538f292ffc78
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-03-09 17:47:46 +0700

    selesai

--- file1.cpp
+++ file1.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int main(){
+    int x;
+
+    srand(time(0));
+    x = rand() % 10;
+
+    if (x==5){
+        x = x+1;
+        cout<<"nilai x sudah dikoreksi menjadi : "<<x<<endl;
+    }
+
+    cout << "Bilangan yang diberikan = "<<x;
+}
