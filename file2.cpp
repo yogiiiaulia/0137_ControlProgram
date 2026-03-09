c5024175d0d39c6d3e9c443d384aa09ef0219a7f
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-03-09 17:54:38 +0700

    membuat cout cin

--- file2.cpp
+++ file2.cpp
@@ -12,6 +12,14 @@ string Status_lulus(float r){
         return "Gagal";
 }
 
-int main(){}
-    
+int main(){
+    float nilBI,nilMT,rerata;
+    string status;
 
+    cout <<"Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout <<"Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    
+}
