bdf0b93751e324b7a1064dd89640484e75d35844
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-03-09 17:50:04 +0700

    membuat fungsi

--- file2.cpp
+++ file2.cpp
@@ -4,3 +4,10 @@ using namespace std;
 float Rata_rata(float a, float b){
     return (a+b)/2;
 }
+
+string Status_lulus(float r){
+    if (r >= 60)
+        return "Lolos";
+    else
+        return "Gagal";
+}
